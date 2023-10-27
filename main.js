6afdd2856443a1d6441585a5cf4113e5496f7a65
danni-cool <contact@danni.cool>
2023-10-27 13:52:43 +0800

    feat: 🎸 提交临时修改patch

--- main.js
+++ main.js
@@ -14,3 +14,21 @@ registerRoute({app, bot})
 app.listen(PORT, () => {
   // console.log(`service is running`);
 });
+
+// const frida = require('frida');
+
+// async function main() {
+//   const deviceManager = frida.getDeviceManager();
+//   const device = await deviceManager.addRemoteDevice('127.0.0.1:27042'); // 连接到 localhost 的 27042 端口
+
+//   // 列出所有进程
+//   const processes = await device.enumerateProcesses();
+//   for (const process of processes) {
+//     console.log(`PID: ${process.pid}, Name: ${process.name}`);
+
+//     if (process.name === 'WeChat.exe') {
+//       const session = await device.attach('wechat.exe'); // 附加到 wechat.exe
+//       console.log('Attached to wechat.exe');
+//     }
+//   }
+// }
